01f72cc1e669c753c828fc3a72002b1df5d32656
SETISAEDU <setisa2016@gmail.com>
2016-08-12 13:53:23 -0600

    Add files via upload
    ServoControl_BT.ino   -Codigo usado para el maestro
    Servo_BT.ino               -Codigo usado para el esclavo
    BT_AT_mode.ino         -Codigo usado para configuras los modulos HC-05

---
+++ codigos arduino/ServoControl_BT/ServoControl_BT.ino
@@ -0,0 +1,79 @@
+//Codigo para el maestro
+int potenciometro; //Variable para guardar el valor analogico
+int pos = 0; //variable de posicion en grados
+
+void setup() {
+  //Rutina de arranque
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(1000);
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(1000);
+  digitalWrite(13, HIGH);
+
+  //Iniciacion del puerto serial para el Bluetooth
+  Serial1.begin(115200);
+
+  //Declaracion de interrupcion de los pulsadores
+  pinMode(2, INPUT);
+  pinMode(3, INPUT);
+  pinMode(20, INPUT);
+  pinMode(21, INPUT);
+  // Interrupciones por flanco ascendente, se recomienda el uso de capacitor extra 
+  // para evitar efectos de rebote
+  attachInterrupt(digitalPinToInterrupt(2), Red, RISING);
+  attachInterrupt(digitalPinToInterrupt(3), Green, RISING);
+  attachInterrupt(digitalPinToInterrupt(20), White, RISING);
+  attachInterrupt(digitalPinToInterrupt(21), Blue, RISING);
+  
+}
+
+void loop() {
+  //Lectura del pin analogico y envio de datos
+  Serial1.print("*"); //Se envia un primer dato para activar el receptor
+  potenciometro = analogRead(0); // Se lee el valor del potenciometro con el ADC del arduino
+  pos = map(potenciometro, 0, 1023, 15, 180); //ajuste de valores maximos y minimos de angulo
+  Serial1.print(String(pos)); // Se envia el valor deposicion
+
+  //Retardo para evitar conflictos
+  delay(100);
+}
+
+void Green() 
+{
+  noInterrupts(); /* Se desactivan las interrupcion para no aceptar nuevas peticiones 
+                   * generadas por los rebotes
+                   */
+  Serial1.print("G"); //Se envia la letra asignada a el pulsador
+  delay(200);  // Espera de estabilizacion del pulsador
+  interrupts(); /* Se reactivan las interrupciones, para aceptar nuevas peticones 
+                 * de los pulsadores
+                 */
+}
+void Red() 
+{
+  noInterrupts();
+  Serial1.print("R");
+  delay(200);
+  interrupts();
+}
+void White() 
+{
+  noInterrupts();
+  Serial1.print("W");
+  delay(200);
+  interrupts();
+}
+void Blue() 
+{
+  noInterrupts();
+  Serial1.print("B");
+  delay(200);
+  interrupts();
+}
+
+
---
+++ codigos arduino/Servo_BT/Servo_BT.ino
@@ -0,0 +1,71 @@
+//Codigo para el esclavo
+//Se agraga la libreria para el uso de servos
+#include <Servo.h>
+
+Servo servo;  // Se crea el objeto para el control del servo
+//Declaracion de pines para los LEDs y Buzzer
+int LED1=2;
+int LED2=3;
+int LED3=4;
+int BUZZER=5;
+//Declaracion de variables
+int pos = 90;    // Variable para el control de la posicion del servo
+
+char BluetoothData;
+//Variables de estado de los LEDs y buzzer
+bool R_state=0;
+bool G_state=0;
+bool Y_state=0;
+bool B_state=0;
+
+
+void setup() {
+   //Inciacion de salidas digitales
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  //Se ponen en alto todas las salidas
+  digitalWrite(LED1,HIGH);
+  digitalWrite(LED2,HIGH);
+  digitalWrite(LED3,HIGH);
+  digitalWrite(BUZZER,HIGH);
+  
+  servo.attach(9);  // Se  define un pin para el objeto
+  //Iniciacion del puerto serial para el Bluetooth
+  Serial1.begin(115200);
+  delay(400);
+  digitalWrite(BUZZER,LOW);
+  digitalWrite(LED1,LOW);//indicador de Arduino listo y configurado
+  digitalWrite(LED2,LOW);
+  digitalWrite(LED3,LOW);
+}
+
+void loop() {
+  //Espera de dato entrante del Bluetooth
+    
+   while(Serial1.available()>0){
+    BluetoothData=Serial1.read(); 
+    //Decision de accion a realizar
+    if(BluetoothData=='R')
+    {
+      R_state=!R_state;  //Cambio de estado de la variable de control del led
+      digitalWrite(LED1,R_state);
+    }else if(BluetoothData=='G')
+    {
+      G_state=!G_state; //Cambio de estado de la variable de control del led
+      digitalWrite(LED2,G_state); 
+    }else if(BluetoothData=='W')
+    {
+      Y_state=!Y_state; //Cambio de estado de la variable de control del led
+      digitalWrite(LED3,Y_state);
+    }else if(BluetoothData=='B')
+    {
+      B_state=!B_state; //Cambio de estado de la variable de control del Buzzer
+      digitalWrite(BUZZER,B_state);
+    }
+    pos=Serial1.parseInt();//Se recupera la siguente cantidad entera, despues del *
+    servo.write(pos); //Se escribe la nueva posicion recibida
+   }   
+}
+
